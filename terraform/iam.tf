3b2d50375e5ffd0d8cb5d4d14e8b113f02af9494
David Bugalho de Moura <David-Xilo@users.noreply.github.com>
2025-07-09 20:35:09 +0200

    hardcode service accounts

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,6 +1,8 @@
 data "google_project" "project" {}
 
 locals {
+  cloud_run_sa_email = "safehouse-cloud-run@${var.project_id}.iam.gserviceaccount.com"
+  terraform_cicd_sa_email = "safehouse-terraform-cicd@${var.project_id}.iam.gserviceaccount.com"
   cloud_run_roles = [
     "roles/cloudsql.client",
     "roles/logging.logWriter",
@@ -35,20 +37,20 @@ resource "google_project_iam_member" "cloud_run_sa_roles" {
   for_each = toset(local.cloud_run_roles)
   project  = var.project_id
   role     = each.value
-  member   = "serviceAccount:${data.google_service_account.cloud_run_sa.email}"
+  member   = "serviceAccount:${local.cloud_run_sa_email}"
 }
 
 resource "google_project_iam_member" "terraform_cicd_roles" {
   for_each = toset(local.cicd_roles)
   project  = var.project_id
   role     = each.value
-  member   = "serviceAccount:${data.google_service_account.terraform_cicd.email}"
+  member   = "serviceAccount:${local.terraform_cicd_sa_email}"
 }
 
 resource "google_service_account_iam_member" "github_workload_identity" {
   for_each = toset(local.allowed_repositories)
 
-  service_account_id = data.google_service_account.terraform_cicd.name
+  service_account_id = "projects/${var.project_id}/serviceAccounts/${local.terraform_cicd_sa_email}"
   role               = "roles/iam.workloadIdentityUser"
   member             = "principalSet://iam.googleapis.com/projects/${data.google_project.project.number}/locations/global/workloadIdentityPools/${google_iam_workload_identity_pool.github_pool.workload_identity_pool_id}/attribute.repository/${var.github_user}/${each.value}"
 }
