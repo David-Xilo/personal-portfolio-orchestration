43af95c5ecaff1ccf519ac9ea89cac642c4990a8
David Bugalho de Moura <David-Xilo@users.noreply.github.com>
2025-07-10 20:55:29 +0200

    remove monitoring resource

--- terraform/monitoring.tf
+++ terraform/monitoring.tf
@@ -28,30 +28,3 @@ resource "google_logging_project_sink" "security_sink" {
 }
 
 
-resource "google_monitoring_alert_policy" "unauthorized_access" {
-  display_name = "Unauthorized Access Attempts"
-  combiner     = "OR"
-
-  conditions {
-    display_name = "High 4xx Error Rate"
-
-    condition_threshold {
-      filter          = "resource.type=\"cloud_run_revision\" AND resource.labels.service_name=\"${google_cloud_run_service.safehouse_backend.name}\""
-      comparison      = "COMPARISON_GT"
-      threshold_value = 10
-      duration        = "300s"
-
-      aggregations {
-        alignment_period   = "60s"
-        per_series_aligner = "ALIGN_RATE"
-      }
-    }
-  }
-
-  notification_channels = [] # Add notification channels here
-
-  alert_strategy {
-    auto_close = "1800s"
-  }
-}
-
