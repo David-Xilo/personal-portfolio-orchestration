cbaad85ad4a7c10dc05645865f87e56c2000d325
David Bugalho de Moura <David-Xilo@users.noreply.github.com>
2025-07-13 13:07:55 +0200

    use proxy for backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -88,6 +88,7 @@ resource "google_cloud_run_service" "safehouse_backend" {
     metadata {
       annotations = {
         "run.googleapis.com/vpc-access-connector" = google_vpc_access_connector.connector.name
+        "run.googleapis.com/cloudsql-instances"   = google_sql_database_instance.db_instance.connection_name
       }
     }
 
