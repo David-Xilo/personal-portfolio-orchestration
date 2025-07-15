424b77ba3aa52a6b27912749d4e2742a2c4060b1
David Bugalho de Moura <David-Xilo@users.noreply.github.com>
2025-07-15 20:20:40 +0200

    fix command syntax

--- terraform/migrations.tf
+++ terraform/migrations.tf
@@ -49,8 +49,8 @@ resource "null_resource" "run_migrations" {
         -e DATABASE_NAME="safehouse_db" \
         -e DATABASE_USER="${google_sql_user.db_user_iam_short.name}" \
         -e USE_IAM_AUTH="true" \
-        -e GOOGLE_ACCESS_TOKEN="\$${TERRAFORM_TOKEN}" \
-        -e DB_ACCESS_TOKEN="\$${DB_ACCESS_TOKEN}" \
+        -e GOOGLE_ACCESS_TOKEN="$$(gcloud auth print-access-token)" \
+        -e DB_ACCESS_TOKEN="$$(gcloud auth print-access-token --impersonate-service-account='${google_service_account.db_access.email}')" \
         -e CONNECTION_NAME="${google_sql_database_instance.db_instance.connection_name}" \
         --network="host" \
         gcr.io/${var.project_id}/safehouse-migrations:${var.migration_image_tag} up
@@ -91,8 +91,8 @@ resource "null_resource" "verify_migration_completion" {
         -e DATABASE_NAME="safehouse_db" \
         -e DATABASE_USER="${google_sql_user.db_user_iam_short.name}" \
         -e USE_IAM_AUTH="true" \
-        -e GOOGLE_ACCESS_TOKEN="\$${TERRAFORM_TOKEN}" \
-        -e DB_ACCESS_TOKEN="\$${DB_ACCESS_TOKEN}" \
+        -e GOOGLE_ACCESS_TOKEN="$$(gcloud auth print-access-token)" \
+        -e DB_ACCESS_TOKEN="$$(gcloud auth print-access-token --impersonate-service-account='${google_service_account.db_access.email}')" \
         -e CONNECTION_NAME="${google_sql_database_instance.db_instance.connection_name}" \
         --network="host" \
         gcr.io/${var.project_id}/safehouse-migrations:${var.migration_image_tag} version
